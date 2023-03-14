20c8452eb419efd8715b10e967ddf25d4245abcd
AndriaKonstantinidi <a97933199@gmail.com>
2023-03-14 18:55:43 +0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int i = 0;
+
+//while(i < 10)
+//{
+//    i++;
+//    if ((i%2)!=0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+
+
+//for(int i = 1; i <= 10; i++)
+//{
+//    if((i%2) == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+var i = new int[50];
+
+for(int j = 1; j < 50; j++)
+{
+    i[j] = j;
+}
+var count = 0;
+foreach (var item in i)
+{
+    if ((item%2)!=0)
+    {
+        count++;
+    }
+    
+}
+Console.WriteLine(count);
